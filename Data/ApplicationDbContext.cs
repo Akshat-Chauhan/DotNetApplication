e6be4e35af3b797c4cbddab458fb3516d3389978
Akshat Chauhan <akshat.chauhan1989@gmail.com>
2023-10-27 11:02:31 +0200

    First commit of application

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DotNetApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetApplication.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {     
+        }
+
+        public DbSet<Category> Categories { get; set; }    
+    }
+}
